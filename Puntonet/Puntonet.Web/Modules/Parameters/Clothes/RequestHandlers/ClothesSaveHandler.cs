6a38ed8311793414285496f13a402c18ec5ad156
Julian <julianmartinez_93@hotmail.com>
2023-06-23 14:23:44 -0300

    add validate is already exist all queryes

--- Puntonet/Puntonet.Web/Modules/Parameters/Clothes/RequestHandlers/ClothesSaveHandler.cs
+++ Puntonet/Puntonet.Web/Modules/Parameters/Clothes/RequestHandlers/ClothesSaveHandler.cs
@@ -1,4 +1,6 @@
-﻿using Serenity.Services;
+using Serenity.Data;
+using Serenity.Services;
+using System;
 using MyRequest = Serenity.Services.SaveRequest<Puntonet.Parameters.ClothesRow>;
 using MyResponse = Serenity.Services.SaveResponse;
 using MyRow = Puntonet.Parameters.ClothesRow;
@@ -12,6 +14,24 @@ namespace Puntonet.Parameters
         public ClothesSaveHandler(IRequestContext context)
              : base(context)
         {
+        }
+
+        protected override void BeforeSave()
+        {
+            base.BeforeSave();
+
+            var existClothe = this.Connection.List<ClothesRow>(
+                    new Criteria(ClothesRow.Fields.Description.ToString().ToUpper()) == Row.Description.ToString()
+                    &
+                    new Criteria(ClothesRow.Fields.IdColor.ToString()) == Row.IdColor.ToString()
+                );
+
+            if (existClothe.Count > 0)
+            {
+                var colorName = this.Connection.ById<ColorsRow>(existClothe[0].IdColor).Description;
+                throw new Exception($"The clothing {Row.Description} is already register with the color {colorName}");
+            }
+
         }
     }
 }
