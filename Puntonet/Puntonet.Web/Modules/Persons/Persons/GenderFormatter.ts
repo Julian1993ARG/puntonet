0eb125f0affda7fb993252e5ccfd7688789956ce
Nehuen Morales <101481465+nehuenmorales@users.noreply.github.com>
2023-06-26 17:50:49 -0300

    icon gender (#9)

--- Puntonet/Puntonet.Web/Modules/Persons/Persons/GenderFormatter.ts
+++ Puntonet/Puntonet.Web/Modules/Persons/Persons/GenderFormatter.ts
@@ -12,9 +12,14 @@ export class GenderFormatter implements Formatter {
         // [1,5,8,4]
         console.log(ctx)
         let id = ctx.value as number;
+
+        console.log("id",id)
         if (!id)
             return "There is an error";
 
-        return '<i class="fa fa-' + ((id == 1) ? 'female text-danger' : 'male text-primary') + '></i>'
+
+        let icon = (id == 2) ? '<i class="fa fa-female text-danger"></i>' : '<i class="fa fa-male text-primary"></i>';
+
+        return icon;
     }
 }
