301d555be30fe63d14aca4b69222c7a849aeeeb6
Julian <85456149+Julian1993ARG@users.noreply.github.com>
2023-06-27 20:43:42 -0300

    finsh (#10)

--- Puntonet/Puntonet.Web/Modules/Persons/Persons/PersonsGrid.ts
+++ Puntonet/Puntonet.Web/Modules/Persons/Persons/PersonsGrid.ts
@@ -1,8 +1,12 @@
-import { Decorators, EntityGrid } from '@serenity-is/corelib';
+import { Decorators, EntityGrid, EnumEditor } from '@serenity-is/corelib';
 import { PersonsColumns, PersonsRow, PersonsService } from '../../ServerTypes/Persons';
 import { PersonsDialog } from './PersonsDialog';
+import { parseQueryString } from '@serenity-is/corelib/q';
+
+const fld = PersonsRow.Fields;
 
 @Decorators.registerClass()
+@Decorators.filterable()
 export class PersonsGrid extends EntityGrid<PersonsRow, any> {
     protected getColumnsKey() { return PersonsColumns.columnsKey; }
     protected getDialogType() { return PersonsDialog; }
@@ -11,9 +15,27 @@ export class PersonsGrid extends EntityGrid<PersonsRow, any> {
     protected getLocalTextPrefix() { return PersonsRow.localTextPrefix; }
     protected getService() { return PersonsService.baseUrl; }
 
+    protected genreStateFilter: EnumEditor;
+
     constructor(container: JQuery) {
         super(container);
     }
+
+    protected createQuickFilters() {
+        super.createQuickFilters();
+        this.genreStateFilter = this.findQuickFilter(EnumEditor, fld.Gender)
+        console.log(this.genreStateFilter, "genreState")
+        this.set_genreState();
+    }
+
+    public set_genreState() {
+        const q = parseQueryString();
+        if (q['genreFilter']) this.genreStateFilter.value = q['genreFilter']
+        console.log(this.genreStateFilter, "genreState")
+        console.log(q, "query")
+    }
+
+
 }
 
 
